38c714027ab00c41333ae804ed6bcc36cf1d9a8d
Thomas Zhu <dev@thomaszhu.com>
2018-05-20 20:59:15 -0700

    Implement song deletion

--- lyrical/client/components/SongList.js
+++ lyrical/client/components/SongList.js
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
+import {
+  compose,
+  graphql,
+} from 'react-apollo';
+import gql from 'graphql-tag';
 import { Link } from 'react-router';
 import PropTypes from 'prop-types';
 
-import query from '../queries/fetchSongs';
+import fetchSongsQuery from '../queries/fetchSongs';
 
 class SongList extends Component {
   static propTypes = {
     data: PropTypes.shape({
       loading: PropTypes.bool.isRequired,
+      refetch: PropTypes.func.isRequired,
       songs: PropTypes.arrayOf(PropTypes.shape({})),
     }).isRequired,
+    mutate: PropTypes.func.isRequired,
   };
   
+  onSongDelete = id => (
+    async () => {
+      const {
+        mutate,
+        data: { refetch },
+      } = this.props;
+
+      await mutate({
+        variables: { id },
+      });
+
+      refetch();
+    }
+  )
+
   renderSongs = () => {
     const { data: { songs } } = this.props;
 
-    return songs.map(song => (
+    return songs.map(({ id, title }) => (
       <li
         className="collection-item"
-        key={song.id}
+        key={id}
       >
-        {song.title}
+        {title}
+
+        <i
+          className="material-icons"
+          onClick={this.onSongDelete(id)}
+          role="presentation"
+        >
+          delete
+        </i>
       </li>
     ));
   }
@@ -49,4 +78,17 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList);
+export default (() => {
+  const mutation = gql`
+    mutation DeleteSong($id: ID) {
+      deleteSong(id: $id) {
+        id
+      }
+    }
+  `;
+
+  return compose(
+    graphql(fetchSongsQuery),
+    graphql(mutation),
+  )(SongList);
+})();
