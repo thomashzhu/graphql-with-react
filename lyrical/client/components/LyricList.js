61e5e0ef7e4fb06b6d90f6a5eb2d6477b59b63e5
Thomas Zhu <dev@thomaszhu.com>
2018-05-23 10:24:18 -0700

    Add ability to like lyric

--- lyrical/client/components/LyricList.js
+++ lyrical/client/components/LyricList.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 
 class LyricList extends Component {
   static propTypes = {
     lyrics: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+    mutate: PropTypes.func.isRequired,
   };
 
+  onLike = (id) => {
+    this.props.mutate({
+      variables: { id },
+    });
+  }
+
   renderLyrics = () => (
-    this.props.lyrics.map(({ id, content }) => (
+    this.props.lyrics.map(({ id, likes, content }) => (
       <li
         className="collection-item"
         key={id}
       >
         {content}
+
+        <div className="vote-box">
+          <i
+            className="material-icons"
+            onClick={() => this.onLike(id)}
+            role="presentation"
+          >
+            thumb_up
+          </i>
+
+          {likes}
+        </div>
       </li>
     ))
   )
@@ -26,4 +47,11 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+export default graphql(gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`)(LyricList);
--- lyrical/client/components/SongDetail.js
+++ lyrical/client/components/SongDetail.js
@@ -40,6 +40,7 @@ export default graphql(gql`
       title
       lyrics {
         id
+        likes
         content
       }
     }
